43565d383d9dbe41a069bb0b8b22fc7e416149ad
Upoma bhuiyan <upomab8901@queenstechhs.org>
2019-01-31 23:14:12 +0000

    d2

--- js/script.js
+++ js/script.js
@@ -1,21 +1,26 @@
 /* global $ */
 
 // BELOW Update the songs array with four of your favorites songs.
-var songs = [];
+var songs = ["Sad","Mic Drop"];
 // BELOW Add 4 More arrays to store images_links, artists, song lengths, and links for each song
 // Make sure they match the same order as your array above
-
+var images_links=[];
+var artists=[];
+var songLengths=[];
 
 
 function displaySongInfo(){
     // BELOW Use forEach Loop to display the data from each of your arrays in the correct div
-
+ songs.forEach(function (song){
+     $("#song").show(song);
+     
+     
+ });
 }
 
 function emptySongInfo(){
     $("#songs").empty();
-    // Use jQuery to empty all of the remaining divs
-
+    
 
 }
 
